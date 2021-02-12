4b9399968c7f67b4ce031a6b4c4176448ee9922f
sefamms <smemis412@gmail.com>
2021-02-12 20:33:10 +0300

    Add project files.

---
+++ ConsoleUII/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager car = new CarManager(new InMemoryCarDal());
+            foreach (var item in car.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>() {
+             new Car{Id=1,ColorId="#aafdv",BrandId="xc",
+                 DailyPrice=200,Description="volvo",ModelYear=2016},
+              new Car{Id=2,ColorId="#fffA",BrandId="",
+                 DailyPrice=200,Description="renault",ModelYear=2018},
+               new Car{Id=3,ColorId="#fafaf",BrandId="1",
+                 DailyPrice=150,Description="fiat",ModelYear=2020},
+                new Car{Id=4,ColorId="#fafafa",BrandId="1",
+                 DailyPrice=200,Description="ford",ModelYear=2015}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            //foreach (var item in _cars)
+            //{
+            //    if (car.Id == item.Id)
+            //    {
+            //        carToDelete = item;
+            //    }
+            //}
+            carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+            //tek tek dolaş eğer benim gönderdiğim id ile eşleşeni sil diyoruz.
+            //genellikle id olanlarda single or default kullanılır.
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            var car = _cars.SingleOrDefault(c => c.Id == id);
+            return car;
+        }
+
+        public void Update(Car car)
+        {
+            Car cartoUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            cartoUpdate.Description = car.Description;
+            cartoUpdate.DailyPrice = car.DailyPrice;
+            cartoUpdate.BrandId = car.BrandId;
+            cartoUpdate.ModelYear = car.ModelYear;
+            cartoUpdate.Id = car.Id;
+        }
+    }
+}
